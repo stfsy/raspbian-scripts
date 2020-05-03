28e879317c68e78fcd4dd5094f74e27c31585ec5
stfsy <s.pfaffel@gmail.com>
2020-05-03 15:59:12 +0200

    chore: add wifi setup scripts

---
+++ setup-wifi/add-wifi-driver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e 
+
+#https://github.com/Mange/rtl8192eu-linux-driver
+declare -r WORKDIR=rtl8192eu-linux-driver
+
+sudo apt-get install git raspberrypi-kernel-headers build-essential
+
+git clone https://github.com/Mange/${WORKDIR}
+
+cd ${WORKDIR}
+
+# add rpi platform
+sed -i '/CONFIG_PLATFORM_I386_PC = y/c\CONFIG_PLATFORM_I386_PC = n\nCONFIG_PLATFORM_ARM_RPI = y' ./Makefile
+
+# enable tdls for i.e. chromecast
+
+# now.. increase max mtu size
+sed -i '/1500/c\1532' include/if_ether.h
+sed -i '/1514/c\1546' include/if_ether.h
+sed -i '/1514/c\1546' include/drv_types_ce.h
+sed -i '/1514/c\1546' include/drv_types_xp.h
+sed -i '/1514/c\1546' include/ethernet.h
+sed -i '/1514/c\1546' include/wifi.h
+
+# change wifi nickname
+sed -i '/<WIFI@REALTEK>/c\rpi-wifi' ./os_dep/linux/ioctl_linux.c
+
+sudo make
+sudo make install
+
+cd ..
+#rm -rf ${WORKDIR}
---
+++ setup-wifi/install-and-configure-access-point.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -e
+
+declare -r CONF_FOLDER="conf"
+declare -r TEMPLATE_CONF_FOLDER="_"${CONF_FOLDER}""
+
+declare -r WIFI_PASSWORD=$1
+declare -r WIFI_NAME=$2
+declare -r ETH_NAME=$(ifconfig | grep enx | cut -d: -f1)
+
+if [ -z "$WIFI_PASSWORD" ]
+then
+  echo "Please pass wifi password as arg 1"
+  exit 1
+fi
+
+if [ -z "$WIFI_NAME" ]
+then
+  echo "Please pass wifi interface name as arg 2"
+  echo "Hint: use $(ifconfig | grep wl | cut -d: -f1)"
+  exit 1
+fi
+
+if [ -z "$ETH_NAME" ]
+then
+  echo "Please pass ethername interface name as arg 3"
+  echo "Hint: use $(ifconfig | grep "^eth\|^enx" | cut -d: -f1)"
+  exit 1
+fi
+
+writeKeyValuePairIfNotSet() {
+  confFile=$1
+  keyValueConf=$2
+  if grep -qe ^"$keyValueConf" "$confFile";
+  then
+    echo "$1 is already set in /etc/sysctl.conf"
+  else
+    sudo echo "$keyValueConf" >> "$confFile"
+  fi
+}
+
+writeSysCtlIfNotSet() {
+  writeKeyValuePairIfNotSet "/etc/sysctl.conf" "$1"
+}
+
+writeDhcpConfIfNotSet() {
+  writeKeyValuePairIfNotSet "/etc/dhcpcd.conf" "$1"
+}
+
+apt-get update && apt-get install dnsmasq hostapd iptables bridge-utils -y
+
+sudo /etc/init.d/hostapd stop
+
+cp "${TEMPLATE_CONF_FOLDER}"/_hostapd.conf ./"${CONF_FOLDER}"/hostapd.conf
+cp "${TEMPLATE_CONF_FOLDER}"/_hostapd ./"${CONF_FOLDER}"/hostapd
+
+cp "${TEMPLATE_CONF_FOLDER}"/_bridge-br0.netdev ./"${CONF_FOLDER}"/bridge-br0.netdev
+cp "${TEMPLATE_CONF_FOLDER}"/_bridge-br0-slave.network ./"${CONF_FOLDER}"/bridge-br0-slave.network
+cp "${TEMPLATE_CONF_FOLDER}"/_bridge-br0.network ./"${CONF_FOLDER}"/bridge-br0.network
+
+sed -i "/WIFI_INTF/c\interface=${WIFI_NAME}" ./"${CONF_FOLDER}"/hostapd.conf
+sed -i "/WIFI_PW/c\wpa_passphrase=${WIFI_PASSWORD}" ./"${CONF_FOLDER}"/hostapd.conf
+sed -i "/WIFI_INTF/c\interface=${WIFI_NAME}" ./"${CONF_FOLDER}"/dnsmasq.conf
+sed -i "/ETH_INTF/c\interface=${ETH_NAME}" ./"${CONF_FOLDER}"/bridge-br0-slave.network
+
+cp ./"${CONF_FOLDER}"/hostapd.conf /etc/hostapd/hostapd.conf
+cp ./"${CONF_FOLDER}"/hostapd /etc/default/hostapd
+cp ./"${CONF_FOLDER}"/dnsmasq.conf /etc/dnsmasq.conf
+
+cp ./"${CONF_FOLDER}"/bridge-br0.netdev /etc/systemd/network/bridge-br0.netdev
+cp ./"${CONF_FOLDER}"/bridge-br0-slave.network /etc/systemd/network/bridge-br0-slave.network
+cp ./"${CONF_FOLDER}"/bridge-br0.network /etc/systemd/network/bridge-br0.network
+
+if grep -qe 1 "/proc/sys/net/ipv4/ip_forward"; 
+then
+  echo "Ip forwarding in /proc/sys/net/ipv4/ip_forward is already enabled"
+else
+  sudo sh -c "echo 1 >> /proc/sys/net/ipv4/ip_forward"
+fi
+
+writeSysCtlIfNotSet "net.ipv4.ip_forward=1"
+writeSysCtlIfNotSet "net.ipv6.conf.all.disable_ipv6=1"
+writeSysCtlIfNotSet "net.ipv6.conf.default.disable_ipv6=1"
+
+writeDhcpConfIfNotSet "denyinterfaces ${WIFI_NAME}"
+writeDhcpConfIfNotSet "denyinterfaces ${ETH_NAME}"
+
+sudo brctl addbr br0
+sudo brctl addif br0 ${ETH_NAME}
+
+sudo systemctl restart systemd-networkd
+sudo /etc/init.d/hostapd restart
+
+sudo update-rc.d hostapd enable
+
+sudo systemctl enable systemd-networkd
+sudo systemctl enable systemd-timesyncd
+
+rm -rf "${CONF_FOLDER}"
