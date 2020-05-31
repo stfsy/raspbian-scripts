023047ff871dd1de35cdb90335a60b3dce25c0a3
stfsy <s.pfaffel@gmail.com>
2020-05-31 17:40:20 +0200

    chore: do not set mtus

--- setup-wifi/add-wifi-driver.sh
+++ setup-wifi/add-wifi-driver.sh
@@ -5,7 +5,7 @@ set -e
 #https://github.com/Mange/rtl8192eu-linux-driver
 declare -r WORKDIR=rtl8192eu-linux-driver
 
-sudo apt-get install git raspberrypi-kernel-headers build-essential
+sudo apt-get install bc build-essential git raspberrypi-kernel-headers
 
 git clone https://github.com/Mange/${WORKDIR}
 
@@ -13,16 +13,6 @@ cd ${WORKDIR}
 
 # add rpi platform
 sed -i '/CONFIG_PLATFORM_I386_PC = y/c\CONFIG_PLATFORM_I386_PC = n\nCONFIG_PLATFORM_ARM_RPI = y' ./Makefile
-
-# enable tdls for i.e. chromecast
-
-# now.. increase max mtu size
-sed -i '/1500/c\1532' include/if_ether.h
-sed -i '/1514/c\1546' include/if_ether.h
-sed -i '/1514/c\1546' include/drv_types_ce.h
-sed -i '/1514/c\1546' include/drv_types_xp.h
-sed -i '/1514/c\1546' include/ethernet.h
-sed -i '/1514/c\1546' include/wifi.h
 
 # change wifi nickname
 sed -i '/<WIFI@REALTEK>/c\rpi-wifi' ./os_dep/linux/ioctl_linux.c
--- setup-wifi/compile-and-add-wifi-driver.sh
+++ setup-wifi/compile-and-add-wifi-driver.sh
@@ -14,17 +14,6 @@ cd ${WORKDIR}
 # add rpi platform
 sed -i '/CONFIG_PLATFORM_I386_PC = y/c\CONFIG_PLATFORM_I386_PC = n\nCONFIG_PLATFORM_ARM_RPI = y' ./Makefile
 
-# enable tdls for i.e. chromecast
-
-# now.. increase max mtu size
-sed -i 's/1500/1532/g' include/if_ether.h
-sed -i 's/1514/1546/g' include/if_ether.h
-sed -i 's/1514/1546/g' include/drv_types_ce.h
-sed -i 's/1514/1546/g' include/drv_types_xp.h
-sed -i 's/1514/1546/g' include/ethernet.h
-sed -i 's/1514/1546/g' include/wifi.h
-sed -i 's/1500/1532/g' core/rtw_mp.c
-
 # change wifi nickname
 sed -i 's/<WIFI@REALTEK>/rpi-wifi/g' ./os_dep/linux/ioctl_linux.c
 
